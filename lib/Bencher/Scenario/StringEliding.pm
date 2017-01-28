118bd41ee0a75205c4b5189d4bfcde886e0f2d5e
perlancar (on pc-home) <perlancar@gmail.com>
2017-01-28 10:32:07 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/StringEliding.pm
@@ -0,0 +1,25 @@
+package Bencher::Scenario::StringEliding;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark string eliding',
+    participants => [
+        {fcall_template => 'String::Elide::Parts::elide(<str>, <len>)'},
+        {fcall_template => 'String::Truncate::elide(<str>, <len>)'},
+        {fcall_template => 'Text::Elide::elide(<str>, <len>)'},
+    ],
+    datasets => [
+        {name=>'strlen=80,len=60', args=>{str=>'a'x80, len=>60}},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
