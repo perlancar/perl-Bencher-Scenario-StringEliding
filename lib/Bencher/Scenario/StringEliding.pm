f32d5537c0bbe047bd09e371d126164946c47c6f
perlancar (on pc-office) <perlancar@gmail.com>
2019-09-11 16:57:40 +0700

    Add participants: String::Elide::Tiny, Text::Truncate

--- lib/Bencher/Scenario/StringEliding.pm
+++ lib/Bencher/Scenario/StringEliding.pm
@@ -11,8 +11,10 @@ our $scenario = {
     summary => 'Benchmark string eliding',
     participants => [
         {fcall_template => 'String::Elide::Parts::elide(<str>, <len>)'},
+        {fcall_template => 'String::Elide::Tiny::elide(<str>, <len>)'},
         {fcall_template => 'String::Truncate::elide(<str>, <len>)'},
         {fcall_template => 'Text::Elide::elide(<str>, <len>)'},
+        {fcall_template => 'Text::Truncate::truncstr(<str>, <len>)'},
     ],
     datasets => [
         {name=>'strlen=80,len=60', args=>{str=>'a'x80, len=>60}},
